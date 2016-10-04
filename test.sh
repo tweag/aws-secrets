0817ff32c00a28eee3690c3930a1cbaf80d1db05
Brian Duggan <brian@promptworks.com>
2016-10-04 09:58:34 -0400

    test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,27 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 cd `dirname $0`
-cd ..
-src=test/test-env
+src=`mktemp`
+cat >> $src <<DONE
+export SECRET_WORD=xyzzy
+export MAGIC_WORDS=squeamish_ossifrage
+export RAND=$RANDOM
+DONE
 dst=`mktemp`
 
+echo '# sending'
 ./send-env -s $src -b bduggan-test-bucket -k an-s3-key -a bduggan-test
+echo '# retrieving'
 ./retrieve-env     -b bduggan-test-bucket -k an-s3-key -a bduggan-test > $dst
 
 if diff -q $src $dst; then
-    echo ok
+    echo "pass"
+    exit 0
 else
-    echo fail
+    echo "fail"
     exit 1
 fi
 
+rm $src
 rm $dst
 
